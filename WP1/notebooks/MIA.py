df621bee556daadc3d77d3da09bd126af1ba1fb1
albacrespi <crespialba@gmail.com>
2022-01-25 17:09:23 +0000

    Cleaned code with Pylint
    No major changes.

--- WP1/notebooks/MIA.py
+++ WP1/notebooks/MIA.py
@@ -1,12 +1,15 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from metricPlots import *
+from metricPlots import plotROC_classifier, plot_prob_test_train
 
 
-def create_mia_data(clf, xtrain, xtest): 
+def create_mia_data(clf, xtrain, xtest):
     """
-    This function predict the probability of train and test data (from building the model split) belonging to their corresponding train or test respectively and it also creates labels.
-    
+    This function predict the probability of train and test data
+    (from building the model split) belonging to their
+    corresponding train or test respectively
+    and it also creates labels.
+
     clf: fitted classifier (original model)
     xtrain: training data (original model)
     xtest: test data (original model)
@@ -24,14 +27,14 @@ def create_mia_data(clf, xtrain, xtest):
         np.zeros((len(xtest), 1), int)
         )
     ).flatten()
-#     miX = miX.reshape(-1, 1) # not needed now we're getting probabilities
     return(miX,miY)
 
 
 def run_membership_inference_attack(clf_name, model, xtrain, xtest, MIA_classifier, MIA_name):
     """
-    This function performs MIA (membership inference attack) and plots metrics.
-    
+    This function performs MIA (membership inference attack)
+    and plots metrics.
+
     clf_name: name of the original model in which MIA is performed.
     model: original model in which MIA is performed.
     xtrain: train data of the original model.
@@ -39,18 +42,16 @@ def run_membership_inference_attack(clf_name, model, xtrain, xtest, MIA_classifi
     MIA_classifier: unfitted classifier for MIA.
     MIA_name: name of the MIA classifier.
     """
-    
-    print('clf', clf_name)
+
     miX, miY = create_mia_data(model, xtrain, xtest)
     mi_train_x, mi_test_x, mi_train_y, mi_test_y = train_test_split(miX, miY, test_size=0.2, stratify=miY)
-    
-    #mia_analysis(model, etc) 
+
     mi_rf = MIA_classifier
     mi_rf.fit(mi_train_x, mi_train_y)
-    
-    plotROC_classifier(mi_rf, mi_test_x, mi_test_y, name+" - MIA "+MIA_name)
+
+    plotROC_classifier(mi_rf, mi_test_x, mi_test_y, clf_name+" - MIA "+MIA_name)
 
     mi_pred_y = mi_rf.predict_proba(mi_test_x)
     mi_pred_train_y = mi_rf.predict_proba(mi_train_x)
 
-    plot_prob_test_train(mi_pred_y, mi_pred_train_y, name+" - MIA "+MIA_name)
+    plot_prob_test_train(mi_pred_y, mi_pred_train_y, clf_name+" - MIA "+MIA_name)
--- WP1/notebooks/metricPlots.py
+++ WP1/notebooks/metricPlots.py
@@ -1,60 +1,58 @@
-import matplotlib.pyplot as plt
 import itertools
-from sklearn.metrics import RocCurveDisplay
+import matplotlib.pyplot as plt
+from sklearn.metrics import auc, RocCurveDisplay
 import numpy as np
-from sklearn.metrics import auc
 
 
-
-def plot_confusion_matrix(name, cm, classes,
+def plot_confusion_matrix(name, confusion_matrix, classes,
                           normalize=False,
                           cmap=plt.cm.Blues,
                           save=None):
     """
     This function plots a confusion matrix for predictions of a given model.
     Name: is the name of the model.
-    cm: is the confusion matrix, e.g. output from confusion_matrix(y_test, y_pred).
+    confusion_matrix: is the confusion matrix, e.g. output from confusion_matrix(y_test, y_pred).
     classes: is the number of classes, e.g. output of range(2).
     normalize: (boolean) whether to normalize the confusion matrix or not. Default False.
     cmap: is the colormap of the confusion matrix, default is cm.Blues.
     save: it save figure to the given path/figname. Default don't save.
     """
     if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        confusion_matrix = confusion_matrix.astype('float') / confusion_matrix.sum(axis=1)[:, np.newaxis]
         print("Normalized confusion matrix")
     else:
         print('Confusion matrix, without normalization')
-    
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+
+    plt.imshow(confusion_matrix, interpolation='nearest', cmap=cmap)
     plt.colorbar()
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
 
     fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
+    thresh = confusion_matrix.max() / 2.
+    for i, j in itertools.product(range(confusion_matrix.shape[0]), range(confusion_matrix.shape[1])):
+        plt.text(j, i, format(confusion_matrix[i, j], fmt),
                horizontalalignment="center",
-               color="white" if cm[i, j] > thresh else "black")
+               color="white" if confusion_matrix[i, j] > thresh else "black")
 
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.title(str(name)+' - Confusion matrix')
-    
+
     if save:
         plt.savefig(save, bbox_inches='tight')
     else:
         plt.show()
-    
-    
+
+
 def plotROC_classifier(clf,
                        X_tmp_test, y_tmp_test,
                        title="ROC curve", save=None):
     """
     This function calculates and plots a ROC AUC curve of a given model.
-    Name is the name of the model.
+
     clf: is the fitted classifier.
     X_tmp_test: test data.
     y_tmp_test: labels of test data.
@@ -70,7 +68,7 @@ def plotROC_classifier(clf,
             clf,
             X_tmp_test,
             y_tmp_test,
-            name="ROC fold {}".format(0),#i),
+            name="ROC fold {}".format(0),
             alpha=0.3,
             lw=1,
             ax=ax,
@@ -112,24 +110,29 @@ def plotROC_classifier(clf,
         ylim=[-0.05, 1.05],
         title=title,
     )
-    ax.legend(loc="lower right")    
-    
+    ax.legend(loc="lower right")
+
     if save:
-        plt.savefig(save, bbox_inches='tight')
+        fig.savefig(save, bbox_inches='tight')
     else:
-        plt.show()
-    
-    
+        fig.show()
+
+
 
 def plot_prob_test_train(pred_test, pred_train, title='Membership probalibilty', save=None):
     """
-    This function plots and histogram of the probability associated with MIA (memebership inference attack).
-    pred_test: predicted probability of the test data (from the MIA split, not from building the model).
-    pred_train: predicted probability of the train data (from the MIA split, not from building the model).
-    title: Set a title for the figure, e.g. specify model name/parameters and MIA etc. Default "Membership probalibilty".
+    This function plots and histogram of the probability associated
+    with MIA (memebership inference attack).
+
+    pred_test: predicted probability of the test data
+    (from the MIA split, not from building the model).
+    pred_train: predicted probability of the train data
+    (from the MIA split, not from building the model).
+    title: Set a title for the figure, e.g. specify model name/parameters
+    and MIA etc. Default "Membership probalibilty".
     save: it save figure to the given path/figname. Default don't save.
     """
-    fig, ax = plt.subplots()
+    #fig, ax = plt.subplots()
     plt.hist(np.array(pred_train).flatten(),  alpha=0.5, bins=20, label='Training Data (Members)',
                 histtype='bar', range=(0, 1))
     plt.hist(np.array(pred_test).flatten(),  alpha=0.5, bins=20, label='Test Data (Non-members)',
@@ -138,9 +141,8 @@ def plot_prob_test_train(pred_test, pred_train, title='Membership probalibilty',
     plt.xlabel('Membership Probability')
     plt.ylabel('Fraction')
     plt.title(title)
-    
+
     if save:
         plt.savefig(save, bbox_inches='tight')
     else:
         plt.show()
-    

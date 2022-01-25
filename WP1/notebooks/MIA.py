7639d9ac1d74ab5b9d9da061bf77dd54e8530cb0
albacrespi <crespialba@gmail.com>
2022-01-25 12:07:02 +0000

    Added .py with MIA functions.

---
+++ WP1/notebooks/MIA.py
@@ -0,0 +1,56 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from metricPlots import *
+
+
+def create_mia_data(clf, xtrain, xtest): 
+    """
+    This function predict the probability of train and test data (from building the model split) belonging to their corresponding train or test respectively and it also creates labels.
+    
+    clf: fitted classifier (original model)
+    xtrain: training data (original model)
+    xtest: test data (original model)
+    """
+    miX = np.concatenate(
+        (
+            clf.predict_proba(xtrain),
+            clf.predict_proba(xtest)
+        )
+    )
+
+    miY = np.concatenate(
+        (
+        np.ones((len(xtrain), 1), int),
+        np.zeros((len(xtest), 1), int)
+        )
+    ).flatten()
+#     miX = miX.reshape(-1, 1) # not needed now we're getting probabilities
+    return(miX,miY)
+
+
+def run_membership_inference_attack(clf_name, model, xtrain, xtest, MIA_classifier, MIA_name):
+    """
+    This function performs MIA (membership inference attack) and plots metrics.
+    
+    clf_name: name of the original model in which MIA is performed.
+    model: original model in which MIA is performed.
+    xtrain: train data of the original model.
+    xtest: test data of the original model.
+    MIA_classifier: unfitted classifier for MIA.
+    MIA_name: name of the MIA classifier.
+    """
+    
+    print('clf', clf_name)
+    miX, miY = create_mia_data(model, xtrain, xtest)
+    mi_train_x, mi_test_x, mi_train_y, mi_test_y = train_test_split(miX, miY, test_size=0.2, stratify=miY)
+    
+    #mia_analysis(model, etc) 
+    mi_rf = MIA_classifier
+    mi_rf.fit(mi_train_x, mi_train_y)
+    
+    plotROC_classifier(mi_rf, mi_test_x, mi_test_y, name+" - MIA "+MIA_name)
+
+    mi_pred_y = mi_rf.predict_proba(mi_test_x)
+    mi_pred_train_y = mi_rf.predict_proba(mi_train_x)
+
+    plot_prob_test_train(mi_pred_y, mi_pred_train_y, name+" - MIA "+MIA_name)

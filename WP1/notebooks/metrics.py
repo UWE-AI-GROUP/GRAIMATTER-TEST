1a5ba24b90a1fe5a0080044997ac78151dfac3f9
Alba Crespi <crespialba+debian@gmail.com>
2022-03-02 11:01:22 +0000

    Disable warnings divided by 0.

--- WP1/notebooks/metrics.py
+++ WP1/notebooks/metrics.py
@@ -1,3 +1,6 @@
+#import numpy as np
+import sys
+import warnings
 from sklearn.metrics import confusion_matrix
 from typing import Any, Iterable, Optional
 from sklearn.metrics import roc_auc_score
@@ -33,20 +36,29 @@ def get_metrics(clf,
     metrics = {}
     y_pred = clf.predict(X_test)
     tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-    metrics['TPR'] = tp / (tp + fn) #true positive rate or recall
-    metrics['FPR'] = fp / (fp + tn) #false positive rate, proportion of negative examples incorrectly classified as positives
-    metrics['FAR'] = fp / (fp + tp) #proportion of things classified as positives that are incorrect, also known as false discovery rate
-    metrics['TNR'] = tn / (tn + fp) #true negative rate or specificity
-    metrics['PPV'] = tp / (tp + fp) #precision or positive predictive value
-    metrics['NPV'] = tn / (tn + fn) #negative predictive value
-    metrics['FNR'] = fn / (tp + fn) #false negative rate
-    metrics['ACC'] = (tp + tn) / (tp + fp + fn + tn) #overall accuracy
-    metrics['Advantage'] = abs(metrics['TPR']-metrics['FPR'])
-    metrics['PLR'] = metrics['TPR'] / metrics['FPR'] #positive likelihood ratio
-    metrics['NLR'] = metrics['FNR'] / metrics['TNR'] #negative likelihood ratio
-    metrics['OR'] = metrics['PLR'] / metrics['NLR'] #odds ratio, the odds ratio is used to find the probability of an outcome of an event when there are two possible outcomes
+    #print('tn', tn, 'fp',fp,'fn', fn,'tp', tp)
+    #np.seterr(divide='ignore')#disable divided by 0 errors
+    
+    if not sys.warnoptions:
+        warnings.simplefilter("ignore")
+        #print('WARNING: warnings have been disabled temprorary. Some metrics can be divided by 0.')
+    
+    metrics['TPR'] = float(tp / (tp + fn)) #true positive rate or recall
+    metrics['FPR'] = float(fp / (fp + tn)) #false positive rate, proportion of negative examples incorrectly classified as positives
+    metrics['FAR'] = float(fp / (fp + tp)) #proportion of things classified as positives that are incorrect, also known as false discovery rate
+    metrics['TNR'] = float(tn / (tn + fp)) #true negative rate or specificity
+    metrics['PPV'] = float(tp / (tp + fp)) #precision or positive predictive value
+    metrics['NPV'] = float(tn / (tn + fn)) #negative predictive value
+    metrics['FNR'] = float(fn / (tp + fn)) #false negative rate
+    metrics['ACC'] = float((tp + tn) / (tp + fp + fn + tn)) #overall accuracy
+    metrics['Advantage'] = float(abs(metrics['TPR']-metrics['FPR']))
+    #metrics['PLR'] = float(metrics['TPR'] / metrics['FPR']) #positive likelihood ratio
+    #metrics['NLR'] = float(metrics['FNR'] / metrics['TNR']) #negative likelihood ratio
+    #metrics['OR'] = metrics['PLR'] / metrics['NLR'] #odds ratio, the odds ratio is used to find the probability of an outcome of an event when there are two possible outcomes
     #calculate AUC of model
     y_pred_proba = clf.predict_proba(X_test)[::,1]
     metrics['AUC'] = roc_auc_score(y_test, y_pred_proba)
+    
+    warnings.simplefilter("default")#enable warnings again
     
     return metrics

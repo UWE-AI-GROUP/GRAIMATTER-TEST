d4a18513c7dd9a0180f82c9f66556f62d1a9d0a8
Alba Crespi <crespialba+debian@gmail.com>
2022-03-04 15:47:35 +0000

    Fixed some default values.

--- WP1/notebooks/metrics.py
+++ WP1/notebooks/metrics.py
@@ -1,6 +1,4 @@
-#import numpy as np
 import sys
-#import warnings
 from sklearn.metrics import confusion_matrix
 from typing import Any, Iterable, Optional
 from sklearn.metrics import roc_auc_score
@@ -44,21 +42,16 @@ def get_metrics(clf,
     y_pred = clf.predict(X_test)
     tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
     #print('tn', tn, 'fp',fp,'fn', fn,'tp', tp)
-    #np.seterr(divide='ignore')#disable divided by 0 errors
     
-    #if not sys.warnoptions:
-    #    warnings.simplefilter("ignore")
-        #print('WARNING: warnings have been disabled temprorary. Some metrics can be divided by 0.')
-    
-    metrics['TPR'] = div(tp,(tp + fn), 0) #true positive rate or recall
-    metrics['FPR'] = div(fp, (fp + tn), 1) #false positive rate, proportion of negative examples incorrectly classified as positives
-    metrics['FAR'] = div(fp, (fp + tp), 1) #False alarm rate, proportion of things classified as positives that are incorrect, also known as false discovery rate
-    metrics['TNR'] = div(tn, (tn + fp), 0) #true negative rate or specificity
+    metrics['TPR'] = round(float(tp/(tp + fn)), 8) #true positive rate or recall
+    metrics['FPR'] = round(float(fp / (fp + tn)), 8) #false positive rate, proportion of negative examples incorrectly classified as positives
+    metrics['FAR'] = div(fp, (fp + tp), 0) #False alarm rate, proportion of things classified as positives that are incorrect, also known as false discovery rate
+    metrics['TNR'] = round(float(tn / (tn + fp)), 8) #true negative rate or specificity
     metrics['PPV'] = div(tp, (tp + fp), 0) #precision or positive predictive value
     metrics['NPV'] = div(tn, (tn + fn), 0) #negative predictive value
-    metrics['FNR'] = div(fn, (tp + fn), 1) #false negative rate
-    metrics['ACC'] = div((tp + tn), (tp + fp + fn + tn), 0) #overall accuracy
-    metrics['F1score'] = 2*(div(metrics['PPV']*metrics['TPR'], (metrics['PPV']+metrics['TPR']), 0))#harmonic mean of precision and sensitivity
+    metrics['FNR'] = round(float(fn / (tp + fn)), 8) #false negative rate
+    metrics['ACC'] = round(float((tp + tn) / (tp + fp + fn + tn)), 8) #overall accuracy
+    metrics['F1score'] = round(float(2*((metrics['PPV']*metrics['TPR']) / (metrics['PPV']+metrics['TPR']), 0)))#harmonic mean of precision and sensitivity
     metrics['Advantage'] = float(abs(metrics['TPR']-metrics['FPR']))
     #metrics['PLR'] = float(metrics['TPR'] / metrics['FPR']) #positive likelihood ratio
     #metrics['NLR'] = float(metrics['FNR'] / metrics['TNR']) #negative likelihood ratio
@@ -67,6 +60,5 @@ def get_metrics(clf,
     y_pred_proba = clf.predict_proba(X_test)[::,1]
     metrics['AUC'] = round(roc_auc_score(y_test, y_pred_proba),8)
     
-    #warnings.simplefilter("default")#enable warnings again
     
     return metrics

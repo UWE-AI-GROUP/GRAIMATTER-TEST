80edef053fb8f5ec7e60fa9cd71b6356d21d3a86
Simon Rogers <simon.rogers@nss.nhs.scot>
2022-03-07 12:31:51 +0000

    more risk prediction

--- WP1/notebooks/risk_prediction.py
+++ WP1/notebooks/risk_prediction.py
@@ -61,17 +61,53 @@ features = target_metrics.drop('dataset', axis=1)\
     .drop(['target_FAR', 'target_PPV', 'full_id'], axis=1)
 
 features.max_depth[features.max_depth.isna()] = 200
-target = mia_metrics.mia_AUC
+target = mia_metrics[['mia_AUC', 'dataset']].copy()
 
 # %%
-# 
+# Random train test split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-train_x, test_x, train_y, test_y = train_test_split(features.drop('dataset', axis=1), target, train_size = 0.4)
+train_x, test_x, train_y, test_y = train_test_split(
+    features.drop('dataset', axis=1),
+    target.drop('dataset', axis=1),
+    train_size = 0.4
+)
 rfr = RandomForestRegressor()
 rfr.fit(train_x, train_y)
 predictions = rfr.predict(test_x)
-plt.plot(predictions, test_y, 'ko', color=[0, 0, 0, 0.2])
+fig, ax = plt.subplots(figsize=(20, 12))
+f_col = 'max_depth'
+import numpy as np
+scatter = plt.scatter(predictions, test_y, c = np.log(test_x[f_col].values), cmap="Set1")
+legend1 = ax.legend(*scatter.legend_elements(),
+                    loc="lower left", title=f'log2 {f_col}')
+ax.add_artist(legend1)
+
 plt.xlabel('Predicted MI AUC')
 plt.ylabel('Actual MIA AUC')
+
+# %% 
+train_proportion = 0.40
+datasets = list(features.dataset.unique())
+for i, test_dataset in enumerate(datasets):
+    train_datasets = datasets.copy()
+    del train_datasets[i]
+    train_x = features[features.dataset.isin(train_datasets)].copy()
+    test_x = features[features.dataset == test_dataset].copy()
+    train_y = target[target.dataset.isin(train_datasets)].copy()
+    train_x, _, train_y, _ = train_test_split(train_x, train_y, train_size = train_proportion)
+    test_y = target[target.dataset == test_dataset].copy()
+    rfr.fit(train_x.drop('dataset', axis=1), train_y.drop('dataset', axis=1))
+    predictions = rfr.predict(test_x.drop('dataset', axis=1))
+    plt.figure()
+    plt.plot(predictions, test_y.drop('dataset', axis=1), 'ko', color=[0, 0, 0, 0.2])
+    plt.xlabel('Predicted MI AUC')
+    plt.ylabel('Actual MIA AUC')
+    plt.title(f'{test_dataset} held out')
+# %%
+temp = list(zip(rfr.feature_names_in_, rfr.feature_importances_))
+temp.sort(key = lambda x: x[1], reverse=True)
+for f, i in temp:
+    print(f, i)
+
 # %%

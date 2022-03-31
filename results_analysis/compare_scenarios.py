7e1468b3b59b6681e681054a9e6d5858d1adb351
Simon Rogers <simon.rogers@nss.nhs.scot>
2022-03-31 10:29:31 +0100

    added notebook for comparing scenarios in a file

--- results_analysis/compare_scenarios.py
+++ results_analysis/compare_scenarios.py
@@ -31,7 +31,7 @@ def filter_df(df, conditions):
 conditions = {
     'dataset': 'mimic2-iaccd',
 }
-metric = 'mia_AUC'
+metric = 'mia_Advantage'
 # %%
 scenario_specifics = {}
 
@@ -69,5 +69,39 @@ for row, dataset in enumerate(datasets):
             plt.xlabel(f'{s1} {metric} minus {s2} {metric}')
             plt.title(f"{conditions['dataset']} {metric} (mean = {diff_vals.mean():.3f})")
             plt.plot([0, 0], plt.ylim(), 'k--')
-plt.savefig(f'{metric}_grid.png')
+plt.savefig(f'{metric}_hist_grid.png')
+# %%
+datasets = ['mimic2-iaccd', 'in-hospital-mortality', 'indian liver']
+nrows = len(datasets)
+n_scenario_pairs = int((3 * (3 - 1)) / 2)
+ncols = n_scenario_pairs
+plt.subplots(nrows=nrows, ncols=ncols, figsize=(20, 16))
+index = 0
+for row, dataset in enumerate(datasets):
+    conditions['dataset'] = dataset
+    for scenario in results_df['scenario'].unique():
+        conditions['scenario'] = scenario
+        scenario_specifics[scenario] = filter_df(results_df, conditions)[['param_id', metric]]
+        scenario_specifics[scenario][f'{scenario}_{metric}'] = scenario_specifics[scenario][metric]
+        scenario_specifics[scenario].drop(metric, axis=1, inplace=True)
+    for s1 in list(scenario_specifics.keys())[:-1]:
+        for s2 in list(scenario_specifics.keys())[1:]:
+            if s1 == s2:
+                continue
+            index += 1
+            diff = pd.merge(
+                scenario_specifics[s1],
+                scenario_specifics[s2],
+                how='inner',
+                on='param_id'
+            )
+            col1 = f'{s1}_{metric}'
+            col2 = f'{s2}_{metric}'
+            diff_vals = diff[col1] - diff[col2]
+            plt.subplot(nrows, ncols, index)
+            plt.plot(diff[col1], diff_vals, 'o', color=[0.7, 0, 0, 0.5])
+            plt.xlabel(col1)
+            plt.ylabel(f'{s1} {metric} minus {s2} {metric}')
+            plt.title(f"{conditions['dataset']}")
+plt.savefig(f'{metric}_scatter_grid.png')
 # %%

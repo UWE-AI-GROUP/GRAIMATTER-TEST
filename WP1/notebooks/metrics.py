6c456ced1615e085349853b929e9d301322339b3
Alba Crespi <acrespi001@dundee.ac.uk>
2022-04-12 10:00:27 +0000

    pylint.

--- WP1/notebooks/metrics.py
+++ WP1/notebooks/metrics.py
@@ -1,11 +1,17 @@
-import sys
+'''
+Calculate metrics.
+'''
+
+from typing import Iterable#, Optional, Any
 from sklearn.metrics import confusion_matrix
-from typing import Any, Iterable, Optional
 from sklearn.metrics import roc_auc_score
 
 VAR_THRESH = 1e-2
 
 def div(x,y, default):
+    '''
+    Solve the problem of division by 0 and round up.
+    '''
     if y!=0:
         return round(float(x/y),8)
     else:
@@ -14,19 +20,19 @@ def div(x,y, default):
 
 
 def get_metrics(clf,
-                X_test:Iterable[float], 
+                X_test:Iterable[float],
                 y_test:Iterable[float]):
     """
     Calculate metrics, including attacker advantage for MIA binary.
     Implemented as Definition 4 on https://arxiv.org/pdf/1709.01604.pdf
     which is also implemented in tensorFlow-privacy https://github.com/tensorflow/privacy
-    
+
     clf: fitted model.
     X_test: test data.
     y_test: test data labels.
-    
+
     returns a dictionary with several metrics.
-    
+
     True positive rate or recall (TPR)
     False positive rate (FPR), proportion of negative examples incorrectly classified as positives
     False alarm rate (FAR), proportion of objects classified as positives that are incorrect, also known as false discovery rate
@@ -42,7 +48,7 @@ def get_metrics(clf,
     y_pred = clf.predict(X_test)
     tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
     #print('tn', tn, 'fp',fp,'fn', fn,'tp', tp)
-    
+
     metrics['TPR'] = round(float(tp/(tp + fn)), 8) #true positive rate or recall
     metrics['FPR'] = round(float(fp / (fp + tn)), 8) #false positive rate, proportion of negative examples incorrectly classified as positives
     metrics['FAR'] = div(fp, (fp + tp), 0) #False alarm rate, proportion of things classified as positives that are incorrect, also known as false discovery rate
@@ -56,8 +62,8 @@ def get_metrics(clf,
     #calculate AUC of model
     y_pred_proba = clf.predict_proba(X_test)[:, 1]
     metrics['AUC'] = round(roc_auc_score(y_test, y_pred_proba),8)
-    
+
     # Add some things useful for debugging / filtering
     metrics['pred_prob_var'] = y_pred_proba.var()
-    
+
     return metrics

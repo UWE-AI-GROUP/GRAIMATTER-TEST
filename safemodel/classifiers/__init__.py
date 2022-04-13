a8a032786573d8650baa75301663712548635b65
Simon Rogers <simon.rogers@nss.nhs.scot>
2022-04-13 12:41:10 +0100

    changes missing from prebious commit

--- safemodel/classifiers/__init__.py
+++ safemodel/classifiers/__init__.py
@@ -1,3 +1,4 @@
 from .safedecisiontreeclassifier import SafeDecisionTreeClassifier
 from .saferandomforestclassifier import SafeRandomForestClassifier
 from .safesvc import SafeSVC
+from .dp_svc import DPSVC

b9d1d2a035a1fcb81e302cd33e0c8ad100d585d5
Simon Rogers <simon.rogers@nss.nhs.scot>
2022-04-13 13:07:16 +0100

    added James metrics file

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -5,6 +5,7 @@ Test the metrics
 import unittest
 import numpy as np
 from attacks.metrics import get_metrics
+from attacks.mia_extremecase import min_max_disc
 
 PREDICTED_CLASS = np.array([0, 1, 0, 0, 1, 1])
 TRUE_CLASS = np.array([0, 0, 0, 1, 1, 1])
@@ -55,10 +56,35 @@ class TestMetrics(unittest.TestCase):
         self.assertAlmostEqual(metrics['F1score'], (8 / 9) / (2 / 3 + 2 / 3))
         self.assertAlmostEqual(metrics['Advantage'], 1 / 3)
         self.assertAlmostEqual(metrics['AUC'], 8 / 9)
-        
-        
-        
 
-        
-        
-        
+class TestExtrete(unittest.TestCase):
+    '''
+    Test the extreme metrics
+    '''
+    def test_extreme_default(self):
+        '''
+        Tets with the dummy data
+        '''
+        pred_probs = DummyClassifier().predict_proba(None)
+        maxd, mind, mmd, _ = min_max_disc(TRUE_CLASS, pred_probs)
+
+        # 10% of 6 is 1 so:
+        # maxd should be 1 (the highest one is predicted as1)
+        # mind should be 0 (the lowest one is not predicted as1)
+        self.assertAlmostEqual(maxd, 1.0)
+        self.assertAlmostEqual(mind, 0.0)
+        self.assertAlmostEqual(mmd, 1.0)
+
+    def test_extreme_higer_prop(self):
+        '''
+        Tets with the dummy data but increase proportion to 0.5
+        '''
+        pred_probs = DummyClassifier().predict_proba(None)
+        maxd, mind, mmd, _ = min_max_disc(TRUE_CLASS, pred_probs, x_prop=0.5)
+
+        # 10% of 6 is 1 so:
+        # maxd should be 1 (the highest one is predicted as1)
+        # mind should be 0 (the lowest one is not predicted as1)
+        self.assertAlmostEqual(maxd, 2 / 3)
+        self.assertAlmostEqual(mind, 1 / 3)
+        self.assertAlmostEqual(mmd, 1 / 3)

9b80e729020947752fa3572987ca016ed625a3cd
Andrew McCarthy <mccarthya@acm.org>
2022-05-04 07:54:38 -0700

    Reverted to remove SafeKeras

--- safemodel/classifiers/__init__.py
+++ safemodel/classifiers/__init__.py
@@ -2,4 +2,3 @@ from .safedecisiontreeclassifier import SafeDecisionTreeClassifier
 from .saferandomforestclassifier import SafeRandomForestClassifier
 from .safesvc import SafeSVC
 from .dp_svc import DPSVC
-from .safekeras import Safe_KerasModel

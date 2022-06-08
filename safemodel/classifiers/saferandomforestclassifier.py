9540dd5765e9c981fb89b5706c112ba2bad19406
Jim Smith <james.smith@uwe.ac.uk>
2022-06-08 17:46:06 +0100

    removed print statements

--- safemodel/classifiers/saferandomforestclassifier.py
+++ safemodel/classifiers/saferandomforestclassifier.py
@@ -37,7 +37,6 @@ class SafeRandomForestClassifier(SafeModel, RandomForestClassifier):
 
         # call the super function to deal with any items that are lists
         msg, disclosive = super().additional_checks(curr_separate, saved_separate)
-        print(f"back from superclass msg={msg}")
         # now the relevant random-forest specific things
         for item in self.examine_seperately_items:
             if item == "base_estimator":
@@ -139,5 +138,4 @@ class SafeRandomForestClassifier(SafeModel, RandomForestClassifier):
                         appears_together.remove(other_record)
 
             k_anon_val[record] = len(appears_together)
-        print(k_anon_val)
         return k_anon_val.min()
